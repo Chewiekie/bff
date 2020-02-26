7d11ed71da72553b8d1c2b95f5939cb67fabcd2b
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-26 00:00:17 -0500

    add

--- src/server/server.js
+++ src/server/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 
 require('./utils/auth/strategies/basic');
 
