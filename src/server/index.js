ca265428ada3431a2df4bdd2b196c9939500528d
Oscar Barajas Tavares <oscar@arepa.dev>
2020-02-26 00:25:05 -0500

    asd

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ require('@babel/register')({
 
 require('asset-require-hook')({
   extensions: ['jpg', 'png', 'gif'],
-  name: '/assets/[hash].[ext]',
+  name: '/public/[hash].[ext]',
 });
 
 require('./server');
